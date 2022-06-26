99d3dca917186a12caa97e784a9d4ec6e4c87069
Waseemalame <waseemalame45@gmail.com>
2022-06-25 20:45:23 -0700

    browser router wrapped in ModalProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ModalProvider } from './context/Modal';
 
 import './index.css';
 import { Provider } from 'react-redux';
@@ -29,9 +30,11 @@ if (process.env.NODE_ENV !== 'production') {
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
