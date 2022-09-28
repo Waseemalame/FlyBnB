1a953b541d2e0d93e5318cbbd57c20f4a9837515
Waseemalame <waseemalame45@gmail.com>
2022-09-27 21:45:28 -0700

    state variables added

--- frontend/src/context/MultiContext.js
+++ frontend/src/context/MultiContext.js
@@ -7,14 +7,25 @@ export default function MultiContextProvider(props){
   const [typesForm, setTypesForm] = useState(true);
   const [categoriesForm, setCategoriesForm] = useState(false);
   const [mapForm, setMapForm] = useState(false);
-
-
+  const [latLng, setLatLng] = useState({});
+  const [closeAddrForm, setCloseAddrForm] = useState(true);
+  const [locationFunc, setLocationFunc] = useState();
+  const [address, setAddress] = useState('')
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
   return (
     <MultiContext.Provider
     value={{
       typesForm, setTypesForm,
       categoriesForm, setCategoriesForm,
-      mapForm, setMapForm
+      mapForm, setMapForm,
+      latLng, setLatLng,
+      closeAddrForm, setCloseAddrForm,
+      address, setAddress,
+      city, setCity,
+      state, setState,
+      country, setCountry
     }}
     >
       {props.children}
