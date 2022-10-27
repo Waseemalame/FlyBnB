587bca2bb29bcc8b39218960cdf81fdb9493c4f4
Waseemalame <waseemalame45@gmail.com>
2022-10-27 00:19:06 -0700

    added styles to footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,13 +5,13 @@ const Footer = () => {
     <div className='footer-container'>
         <div className='footer-left'>
             <a href='https://github.com/Waseemalame/FlyBnB/'>
-            <img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" alt='github'/>
+            <img src="https://img.icons8.com/ios-glyphs/40/000000/github.png" alt='github'/>
             </a>
 
         </div>
         <div className='footer-middle'>
             <a href='https://www.linkedin.com/in/waseemalame/'>
-            <img src="https://img.icons8.com/ios-filled/60/000000/linkedin-circled--v1.png" alt='linkedin'/>
+            <img src="https://img.icons8.com/ios-filled/40/000000/linkedin-circled--v1.png" alt='linkedin'/>
             </a>
         </div>
     </div>
