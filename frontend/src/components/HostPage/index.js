918356631bdf2f695a5d0aad25ca77cbf8963369
Waseemalame <waseemalame45@gmail.com>
2022-11-08 16:35:59 -0800

    removed unused code

--- frontend/src/components/HostPage/index.js
+++ frontend/src/components/HostPage/index.js
@@ -1,13 +1,9 @@
-import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import Geocode from "react-geocode";
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { useMultiContext } from '../../context/MultiContext'
-import { addListingThunk } from '../../store/listing';
-import HostForm from '../HostForm'
-import NewHostForm from '../HostForm/NewHostForm'
 import Form from '../MultiForm';
 import './HostPage.css'
 const HostPage = () => {
@@ -16,41 +12,12 @@ const HostPage = () => {
   const user = useSelector(state => state.session.user)
   const [page, setPage] = useState(0);
 
-  const { typesForm, setTypesForm,
-          categoriesForm, setCategoriesForm,
-          mapForm, setMapForm, latLng, setLatLng,
-          closeAddrForm, setCloseAddrForm,
-          type, setType,
-          categoryId, setCategoryId,
-          address, setAddress,
-          city, setCity,
-          state, setState,
-          country, setCountry,
-          guests, setGuests,
-          beds, setBeds,
-          bedrooms, setBedrooms,
-          baths, setBaths,
-          addrErrors, setAddrErrors,
-          errorValidations, setErrorValidations,
-          imageErrors, setImageErrors,
-          infoForm, setInfoForm,
-          amenitiesForm, setAmenitiesForm,
-          amenities, setAmenities,
-          imageForm, setImageForm,
-          images, setImages,
-          priceForm, setPriceForm,
-          price, setPrice,
-          cleaningFee, setCleaningFee,
-          serviceFee, setServiceFee,
-
-          titleForm, setTitleForm, title, setTitle } = useMultiContext()
+  const { typesForm, mapForm, imageForm} = useMultiContext()
 
   const [showBackBtn, setShowBackBtn] = useState(false);
-  const [showImageForm, setShowImageForm] = useState(false)
   const body = document.querySelector('body')
   const rightPage = document.querySelector('.right-page')
 
-  const dispatch = useDispatch()
   const history = useHistory()
 
   useEffect(() => {
@@ -58,6 +25,7 @@ const HostPage = () => {
 
     return () => body.style.overflowY = 'scroll'
   }, []);
+
   useEffect(() => {
     if(imageForm && rightPage){
       rightPage.style.overflowY = 'hidden'
@@ -87,79 +55,25 @@ const HostPage = () => {
   }, [typesForm]);
 
 
-  const getFullAddress = async() => {
-    const errors = []
+//   const getFullAddress = async() => {
+//     const errors = []
 
-    try{
+//     try{
 
-      const res = await Geocode.fromAddress(`${address} ${city}`)
-      const { lat, lng } = res.results[0].geometry.location;
-      setLatLng({
-        lat: lat,
-        lng: lng
-      })
-    }
-    catch (e){
-      setAddrErrors(errors)
-      return 'error'
-    }
+//       const res = await Geocode.fromAddress(`${address} ${city}`)
+//       const { lat, lng } = res.results[0].geometry.location;
+//       setLatLng({
+//         lat: lat,
+//         lng: lng
+//       })
+//     }
+//     catch (e){
+//       setAddrErrors(errors)
+//       return 'error'
+//     }
 
-}
+// }
 
-  // Submit Listing Function
-  const handleSubmitListing = async() => {
-    if(imageErrors.length > 0) return;
-    let amenArr = Object.values(amenities).map(amenity => amenity.name)
-    let imageArr = []
-    if (images && images.length !== 0) {
-      for (let i = 0; i < images.length; i++) {
-        imageArr.push(images[i]);
-      }
-    }
-    const data = {
-      userId: user.id,
-      type,
-      categoryId,
-      address,
-      city,
-      state,
-      country,
-      title,
-      guests,
-      beds,
-      bedrooms,
-      baths,
-      amenities: amenArr,
-      price,
-      cleaningFee,
-      serviceFee,
-      images,
-    }
-    dispatch(addListingThunk(data))
-    setMapForm(false)
-    setTypesForm(true)
-    setCategoriesForm(false)
-    setImageForm(false)
-    setAmenitiesForm(false)
-    setPriceForm(false)
-    setPrice(0)
-    setTitle('')
-    setType('')
-    setCategoryId('')
-    setState('')
-    setCity('')
-    setCountry('')
-    setAddress('')
-    setBeds(0)
-    setBaths(0)
-    setBedrooms(.5)
-    setCleaningFee(0)
-    setServiceFee(0)
-    setGuests(0)
-    setAmenities([])
-    history.push('/')
-
-  }
   return (
     <div class="host-page-container">
 
@@ -200,17 +114,6 @@ const HostPage = () => {
             <div className="right-page">
             <Form page={page} setPage={setPage} />
             </div>
-            {/* <div className="next-page">
-              {showBackBtn && (
-                <button onClick={handleBack} className="back-button" type='button'>Back</button>
-              )}
-                {!imageForm ? (
-                  <button onClick={handleNext} className="next-button" type='button'>Next</button>
-
-                ) :(
-                  <button onClick={handleSubmitListing} className="submit-button" type='button'>Submit</button>
-                )}
-            </div> */}
     </div>
   )
 }
